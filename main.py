2a0818355aae3c0219984d634d57abafcc59bdc3
NikaBezhiashvili <74880697+NikaBezhiashvili@users.noreply.github.com>
2023-11-21 04:29:41 +0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-from Utils.Process.dataframes import userTable
-from Utils.Process.process import user_table_cleaning, store_user_table
+from Utils.Decorators.decorators import check_data_exists
 
 if __name__ == '__main__':
-    store_user_table('Utils/Cleaning/user_table_final.csv',
-                     user_table_cleaning('Utils/RawData/user_table.csv', userTable))
+    #
+    # @check_data_exists
+    # def test(filename):
+    #     pass
+    #
+    # test('C:/Users/nikab/PycharmProjects/Lectures/01_Asignment/RawData/user_table.csv')
+    print('hello')
+
